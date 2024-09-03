efc15f5c72e2aaae6ab2d1765e61772c50f58763
prashanthi <prashmudepaka@gmail.com>
2024-09-03 18:46:02 +0530

    call by reference function

--- Functions/CallByValue.cpp
+++ Functions/CallByValue.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
-void sum(int a,int b){
+void sum(int &a,int &b){
     cout<<"a,b="<<a<<","<<b<<"\n";
     cout<<"sum="<<a+b;
     
 }
 int main(){
-    sum(5,10);
+    int a=4;
+    int b=32;
+    sum(a,b);
     return 0;
 }
