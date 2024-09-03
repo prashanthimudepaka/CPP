6d49e2bf3367d4c554a6070ae3bacf123cb9db91
prashanthi <prashmudepaka@gmail.com>
2024-09-03 18:27:25 +0530

    Do while loop

---
+++ ControlStructures/DoWhile.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=-1; 
+    do
+    {
+        cout<<n<<"\n";
+        n++;
+    }
+    while(n>0); 
+    cout<<"n="<<n;   
+    return 0;
+}
